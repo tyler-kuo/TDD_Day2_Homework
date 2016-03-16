aa48517effe559864bb6152db7ae29a724da8aa2
Tyler <m04875761@gmail.com>
2016-03-16 20:56:42 +0800

    Scenario6: 一二集各買了一本，第三集買了兩本，價格應為100*3*0.9 + 100 = 370（綠燈）

--- TDD_Day2_Homework/main.swift
+++ TDD_Day2_Homework/main.swift
@@ -14,27 +14,46 @@ class Order{
     
     func getAmount(products : Array<HarryPotterBook>) -> Double{
         
-        
-        let count = products.count
-        
-        var preferential = 1.0
-        
-        switch count{
-        case 2:
-            preferential = 0.95
-        case 3:
-            preferential = 0.9
-        case 4:
-            preferential = 0.8
-        case 5:
-            preferential = 0.75
-        default:
-            preferential = 1.0
+        var bookgroupCounts = [Int]()
+        var amount = 0.0
+        for product in products{
+            for index in 1...product.count{
+                if bookgroupCounts.count < index{
+                    bookgroupCounts.append(1)
+                }else{
+                    bookgroupCounts[index - 1] += 1
+                }
+            }
         }
         
         
         
-        return Double(count) * 100.0 * preferential
+        
+        for bookgroupCount in bookgroupCounts{
+        
+            let count = bookgroupCount
+            var preferential = 1.0
+            switch count{
+            case 2:
+                preferential = 0.95
+            case 3:
+                preferential = 0.9
+            case 4:
+                preferential = 0.8
+            case 5:
+                preferential = 0.75
+            default:
+                preferential = 1.0
+            }
+            
+            amount += Double(bookgroupCount) * 100.0 * preferential
+
+        }
+        
+        
+        
+        
+        return amount
     }
     
 }

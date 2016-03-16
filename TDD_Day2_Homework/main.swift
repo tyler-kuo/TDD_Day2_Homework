638e88db13d221c634f4be0b4094568e74eb6c83
Tyler <m04875761@gmail.com>
2016-03-16 20:20:30 +0800

    Initial Commit

---
+++ TDD_Day2_Homework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TDD_Day2_Homework
+//
+//  Created by Tyler on 2016/3/16.
+//  Copyright © 2016年 Tyler. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
